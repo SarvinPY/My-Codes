e58959e731946ace41098740ca8a839f4a2e931d
sarvin <151523715+SarvinPY@users.noreply.github.com>
2024-06-09 12:28:14 +0330

    Delete projects/server.py

--- projects/server.py
+++
@@ -1,9 +0,0 @@
-import socket
-tcpsocket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)  #IPV4 , TCP
-tcpsocket.bind(("192.168.194.155" , 1500))   #IP , Port
-tcpsocket.listen(5)   #Number of clients , that can connect to server
-client , addr = tcpsocket.accept()  #accept method, returns -Two Data- , client and address(which is Tuple and has client IP AND client Port)
-client.send(b"Hello TCP-Server")
-print(f'connected to => {addr}')
-#sending bite
-client.close()
