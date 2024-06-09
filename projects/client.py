a756a59b33754ce86fdf2cefea3496e1da87ef1a
sarvin <151523715+SarvinPY@users.noreply.github.com>
2024-06-09 12:27:45 +0330

    Delete projects/client.py

--- projects/client.py
+++
@@ -1,5 +0,0 @@
-import socket
-sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)  #IPV4 , TCP
-sock.connect("0.0.0.0" , 1500)   #Server Address
-sock.sendall(b'Hello TCP-Client')
-sock.close()
